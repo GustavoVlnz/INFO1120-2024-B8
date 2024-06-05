f4aed92a65e164049f400625e94d30278feca3f2
gustavovlnz <gvalenzuela2023@alu.uct.cl>
2024-06-05 19:49:20 -0400

    trabajo corregido
    vamos en el punto 4

--- main.py
+++ main.py
@@ -6,22 +6,13 @@ import pandas as pd
 
 
 conexion=sqlite3.connect("./plantilla_word/db_personas.db")
-df=pd.read_sql_query("SELECT  personas.nombre_completo,Salarios.rol FROM personas  INNER JOIN  Salarios on personas.id_rol=salarios.id_salarios ",conexion)
+df=pd.read_sql_query("SELECT * FROM personas INNER JOIN Salarios ON personas.id_rol=Salarios.id_salarios",conexion)
 
-
-
-print(df)
-
-
-
-dataframe=pd.read_sql_query("SELECT *  FROM personas join salarios", conexion)
-
-print(dataframe)
 conexion.close()
 
 
 
-filter.singular_data_to_contract(dataframe,1)
+filter.singular_data_to_contract(df,47)
 
 
 
